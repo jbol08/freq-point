50e9076cd43365db9507222eb3574bfa462564c8
Jose Bolanos <bolanosjjr@gmail.com>
2021-05-09 14:16:42 -0500

    average done

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr,nums) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start < end) {
+        let average = (arr[start] + arr[end]) / 2;
+        if (average === nums) {
+            return true;
+        } else if (average < nums) {
+            start++;
+            
+        } else {
+            end--;
+        }    
+    }
+    return false;
 }

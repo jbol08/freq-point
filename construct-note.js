70c85d3f8212398f4c62d3c770ea36b2379783e7
Jose Bolanos <bolanosjjr@gmail.com>
2021-05-09 13:37:10 -0500

    note done

--- construct-note.js
+++ construct-note.js
@@ -1,12 +1,23 @@
 // add whatever parameters you deem necessary
-function constructNote(str1,str2) {
-    for (let char in str2) {
-        if (!str1[char]) {
-            return false;
-        }
-        if (str1[char] > str2[char]) {
-            return false;
-        }
-        return true;
+function constructNote(str2, str1) {
+    let str1Freq = {};
+    let str2Freq = {};
+
+    for (let char of str1) {
+        str1Freq[char] = ++str1Freq[char] || 1;
     }
+    for (let char of str2) {
+        str2Freq[char] = ++str2Freq[char] || 1;
+    }
+    for (let char in str2Freq) {
+        if (!str1Freq[char]) {
+            return false;
+        }
+        if (str2Freq[char] > str1Freq[char]) {
+            return false;
+        }
+    }
+    return true;
 }
+
+module.exports = constructNote;

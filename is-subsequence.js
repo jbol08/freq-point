5569bb4e3e8350a6f18cc9d7965048ed4e7ea9af
Jose Bolanos <bolanosjjr@gmail.com>
2021-05-10 13:12:29 -0500

    subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2,m,n) {
+    if (m === 0)
+        return true;
+    if (n === 0)
+        return false;
+          
+    // If last characters of two strings
+    // are matching
+    if (str1[m - 1] === str2[n - 1])
+        return isSubSequence(str1, str2,
+                             m - 1, n - 1);
+ 
+    // If last characters are not matching
+    return isSubSequence(str1, str2, m, n - 1);
 }

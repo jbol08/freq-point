f9a43d921ae12a71c47532e18684f65c9173d5e8
Jose Bolanos <bolanosjjr@gmail.com>
2021-05-09 21:38:10 -0500

    separate

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start < end) {
+        if (nums[start] < 0 && nums[end] > 0) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+        } else {
+            if (nums[start] > 0) start = start + 1;
+            else end = end - 1;
+        }
+
+    }
+    return nums;
 }
